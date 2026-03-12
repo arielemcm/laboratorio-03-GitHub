021e74681958fa15498098a428dedbec680dab38
Carlos David Castanon Mendez <cardavcm@gmail.com>
2026-03-12 12:43:37 -0400

    resuelve el problema operadores relacionales 1018 del desarrollador 1

---
+++ Main.java
@@ -0,0 +1,25 @@
+package javajuez;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+
+            if (a > b) {
+                System.out.println(">");
+            } else if (a < b) {
+                System.out.println("<");
+            } else {
+                System.out.println("=");
+            }
+        }
+
+        sc.close();
+    }
+}
+
